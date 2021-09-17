6fa9f8714e633b2d6dc1118d6148c17137fef6fb
Roger Martin <rdmartin33@hotmail.com>
2021-09-17 10:28:52 -0600

    Add project files.

---
+++ GasTrackerMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GasTrackerMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
